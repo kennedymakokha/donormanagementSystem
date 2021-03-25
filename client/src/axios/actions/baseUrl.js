52c25afc05023fbd289b0947cd51efeca90c3f8f
kenate <katchibo2@gmail.com>
2021-03-25 21:35:13 +0300

    add apply button

--- client/src/axios/actions/baseUrl.js
+++ client/src/axios/actions/baseUrl.js
@@ -1,2 +1,2 @@
-export const base0 = 'http://157.230.4.144:1400';
-export const base= 'http://localhost:1400';
+export const base = 'http://157.230.4.144:1400';
+export const base0= 'http://localhost:1400';
